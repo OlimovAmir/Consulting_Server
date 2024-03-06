62e57202bec811e2d9ee2ea7239c3b6ea9737e98
olimov Amir <olimov.amir@mail.ru>
2024-03-06 20:34:39 +0300

    fix

--- Repositories/IPostgreSQLRepository.cs
+++ Repositories/IPostgreSQLRepository.cs
@@ -4,5 +4,10 @@ namespace Consulting_Server.Repositories
 {
     public interface IPostgreSQLRepository<T> where T : BaseEntity
     {
+        IQueryable<T> GetAll();
+        T GetById(Guid id);
+        bool Create(T item);
+        bool Update(T item);
+        bool Delete(Guid id);
     }
 }
