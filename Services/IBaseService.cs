9b8ba67458b70f61098591aaf5161385b00f5d11
olimov Amir <olimov.amir@mail.ru>
2024-03-06 21:22:32 +0300

    fix

--- Services/IBaseService.cs
+++ Services/IBaseService.cs
@@ -1,6 +1,17 @@
-﻿namespace Consulting_Server.Services
+﻿using Consulting_Server.Models.BaseModels;
+
+namespace Consulting_Server.Services
 {
-    public interface IBaseService
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
     {
+        IQueryable<TEntity> GetAll();
+
+        TEntity GetById(Guid id);
+
+        string Create(TEntity item);
+
+        string Update(Guid id, TEntity item);
+
+        string Delete(Guid id);
     }
 }
