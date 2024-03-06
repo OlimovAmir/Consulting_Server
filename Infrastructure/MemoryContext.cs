93a7d086e8cfcd8218d5591f6f4a0532e2a85e82
olimov Amir <olimov.amir@mail.ru>
2024-03-06 21:13:41 +0300

    fix

--- Infrastructure/MemoryContext.cs
+++ Infrastructure/MemoryContext.cs
@@ -4,6 +4,10 @@ namespace Consulting_Server.Infrastructure
 {
     public class MemoryContext : DbContext
     {
-
+        public MemoryContext(DbContextOptions<MemoryContext> options) : base(options)
+        {
+            Database.Migrate();
+            Database.EnsureCreated();
+        }
     }
 }
