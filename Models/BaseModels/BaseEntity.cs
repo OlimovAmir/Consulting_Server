5345d4eb685ab38537eff17297ce8019cc9f7e77
olimov Amir <olimov.amir@mail.ru>
2024-03-06 20:22:13 +0300

    fix

--- Models/BaseModels/BaseEntity.cs
+++ Models/BaseModels/BaseEntity.cs
@@ -2,5 +2,16 @@
 {
     public abstract class BaseEntity
     {
+        public Guid Id { get; }
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override string ToString()
+        {
+            return $"Id:{Id} ({GetType().Name})";
+        }
     }
 }
