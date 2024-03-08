3cca5c0a44477ee23c7c1e5b4ca5bb39d425598a
olimov Amir <olimov.amir@mail.ru>
2024-03-08 19:58:16 +0300

    fix

--- Services/MessageFromUserService.cs
+++ Services/MessageFromUserService.cs
@@ -1,9 +1,16 @@
 ﻿using Consulting_Server.Models;
+using Consulting_Server.Repositories;
 
 namespace Consulting_Server.Services
 {
     public class MessageFromUserService : IMessageFromUserService
     {
+        IPostgreSQLRepository<MessageFromUser> _repository;
+
+        public MessageFromUserService(IPostgreSQLRepository<MessageFromUser> repository)
+        {
+            _repository = repository;
+        }
         public string Create(MessageFromUser item)
         {
             throw new NotImplementedException();
