47a5c9658f80d42f01e3011acff484b2fc3a2b2f
olimov Amir <olimov.amir@mail.ru>
2024-03-08 19:33:21 +0300

    fix

--- Models/MessageFromUser.cs
+++ Models/MessageFromUser.cs
@@ -1,8 +1,11 @@
 ﻿using Consulting_Server.Models.BaseModels;
+using System.Net;
+using System.Net.Mail;
 
 namespace Consulting_Server.Models
 {
     public class MessageFromUser: BaseEntity
     {
+
     }
 }
