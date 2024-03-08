4da41e66f2d51007cb0e2b916d879bd23b996028
olimov Amir <olimov.amir@mail.ru>
2024-03-08 19:44:37 +0300

    fix

--- Models/MessageFromUser.cs
+++ Models/MessageFromUser.cs
@@ -6,6 +6,22 @@ namespace Consulting_Server.Models
 {
     public class MessageFromUser: BaseEntity
     {
+        public void SendEmail(string recipientEmail, string messageBody)
+        {
+            using (var smtpClient = new SmtpClient("smtp.example.com"))
+            {
+                smtpClient.Port = 587;
+                smtpClient.Credentials = new NetworkCredential("your_smtp_username", "your_smtp_password");
+                smtpClient.EnableSsl = true;
 
+                using (var message = new MailMessage())
+                {
+                    message.To.Add(recipientEmail);
+                    message.Body = messageBody;
+
+                    smtpClient.Send(message);
+                }
+            }
+        }
     }
 }
