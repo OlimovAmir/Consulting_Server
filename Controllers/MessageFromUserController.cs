ea93549cd536d2f2cdc317d45ad7f9c2b91b56aa
olimov Amir <olimov.amir@mail.ru>
2024-03-08 20:25:04 +0300

    fix

---
+++ Controllers/MessageFromUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Consulting_Server.Controllers
+{
+    public class MessageFromUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
